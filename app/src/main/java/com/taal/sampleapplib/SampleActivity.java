c7e16444a510da4c1d32fb3494f229e4d7c1413f
Prathap Badavath <prathap.badavath@gmail.com>
2019-11-28 20:53:22 +0530

    removed un necessary code.

--- app/src/main/java/com/taal/sampleapplib/SampleActivity.java
+++ app/src/main/java/com/taal/sampleapplib/SampleActivity.java
@@ -5,13 +5,15 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import com.taal.felijandroidlib.MainActivity;
+
+public class SampleActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        startActivity(new Intent(this, com.taal.felijandroidlib.MainActivity.class));
+        startActivity(new Intent(this, MainActivity.class));
         finish();
     }
 }
